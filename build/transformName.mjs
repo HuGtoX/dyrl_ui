556d8faa56a997cff42a9d45bcc3c2aee81c6bfc
hua <1070856116@qq.com>
2024-10-15 19:21:57 +0800

    代码转换和文件重命名
    - 添加 transformName 函数以转换文件名
    - 使用 fileURLToPath 和 dirname 实现 __dirname 功能
    - 重命名多个文件夹和文件以符合新的命名约定
    - 更新 global.d.ts 中的组件引用路径
    - 修改 Vite 配置中的 root 路径

---
+++ build/transformName.mjs
@@ -0,0 +1,45 @@
+import path, { basename } from "path";
+import { fileURLToPath } from "url";
+import { readdir, rename } from "node:fs/promises";
+
+function transformName(name) {
+  const comName = Array.from(name)
+    .reduce((acc, char, index) => {
+      if (/[A-Z]/.test(char)) {
+        if (index === 0) {
+          acc.push(char.toLowerCase());
+        } else {
+          acc.push("-" + char.toLowerCase());
+        }
+      } else {
+        acc.push(char);
+      }
+      return acc;
+    }, [])
+    .join("");
+
+  return comName;
+}
+
+const originPath = path.dirname(fileURLToPath(import.meta.url));
+const comDirname = path.join(
+  originPath,
+  "../src/uni_modules/dyrl_ui/components"
+);
+console.log("--- originPath ---", originPath);
+console.log("--- comDirname ---", comDirname);
+
+try {
+  const files = await readdir(comDirname);
+  for (const file of files) {
+    const name = transformName(file);
+    try {
+      await rename(path.join(comDirname, file), path.join(comDirname, name));
+      console.log(`文件夹已从 "${file}" 重命名为 "${name}"`);
+    } catch (error) {
+      console.error(`重命名文件夹失败: ${error.message}`);
+    }
+  }
+} catch (err) {
+  console.error(err);
+}
--- src/uni_modules/dyrl_ui/global.d.ts
+++ src/uni_modules/dyrl_ui/global.d.ts
@@ -1,21 +1,21 @@
 declare module "vue" {
   // Helper for Volar
   export interface GlobalComponents {
-    RlButton: (typeof import("./components/Button/index"))["default"];
-    RlCell: (typeof import("./components/Cell/index"))["default"];
-    RlImage: (typeof import("./components/Image/index"))["default"];
-    RlIcon: (typeof import("./components/Icon/index"))["default"];
-    RlCard: (typeof import("./components/Card/index"))["default"];
-    RlSection: (typeof import("./components/Section/index"))["default"];
-    RlSpace: (typeof import("./components/Space/index"))["default"];
-    RlGrid: (typeof import("./components/Grid/index"))["default"];
-    RlGridItem: (typeof import("./components/GridItem/index"))["default"];
-    RlText: (typeof import("./components/Text/index"))["default"];
-    RlGap: (typeof import("./components/Gap/index"))["default"];
-    RlPopup: (typeof import("./components/Popup/index"))["default"];
-    RLNavbar: (typeof import("./components/Navbar/index"))["default"];
-    RLRadioGroup: (typeof import("./components/Radio-Group/index"))["default"];
-    RLRadio: (typeof import("./components/Radio/index"))["default"];
+    RlButton: (typeof import("./components/button/index"))["default"];
+    RlCell: (typeof import("./components/cell/index"))["default"];
+    RlImage: (typeof import("./components/image/index"))["default"];
+    RlIcon: (typeof import("./components/icon/index"))["default"];
+    RlCard: (typeof import("./components/card/index"))["default"];
+    RlSection: (typeof import("./components/section/index"))["default"];
+    RlSpace: (typeof import("./components/space/index"))["default"];
+    RlGrid: (typeof import("./components/grid/index"))["default"];
+    RlGridItem: (typeof import("./components/grid-item/index"))["default"];
+    RlText: (typeof import("./components/text/index"))["default"];
+    RlGap: (typeof import("./components/gap/index"))["default"];
+    RlPopup: (typeof import("./components/popup/index"))["default"];
+    RLNavbar: (typeof import("./components/navbar/index"))["default"];
+    RLRadioGroup: (typeof import("./components/radio-group/index"))["default"];
+    RLRadio: (typeof import("./components/radio/index"))["default"];
   }
 }
 
